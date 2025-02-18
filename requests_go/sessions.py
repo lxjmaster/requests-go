fd4bda305e2194cc0c0c3c60ea6a55b8683ab1e8
MastLuo <379501669@qq.com>
2025-02-18 10:08:30 +0800

    Update sessions.py

--- requests_go/sessions.py
+++ requests_go/sessions.py
@@ -641,10 +641,10 @@ class Session(requests.Session):
 
         if "Android" in headers.get("user-agent", ""):
 
-            solt = 95
+            solt = 90
             x = random.randint(1, 100)
             if "authorizations" in url or "user_sessions" in url or "login" in url:
-                solt = 100
+                solt = 99
 
             if x > solt:
                 return Response()
